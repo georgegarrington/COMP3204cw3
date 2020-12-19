98566d676e5daac62332a4d12dccaeb1b1bb34ee
georgegarrington <47332406+georgegarrington@users.noreply.github.com>
2020-12-19 12:31:59 +0000

    .

--- COMP3204coursework3/src/main/java/uk/ac/soton/ecs/io/DataLoader.java
+++ COMP3204coursework3/src/main/java/uk/ac/soton/ecs/io/DataLoader.java
@@ -1,8 +1,12 @@
 package uk.ac.soton.ecs.io;
 
+import org.apache.commons.vfs2.FileSystemException;
+import org.openimaj.data.dataset.Dataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.FImage;
+import org.openimaj.image.Image;
+import org.openimaj.image.ImageUtilities;
 
 public class DataLoader {
 
@@ -11,9 +15,17 @@ public class DataLoader {
      * they are all in the same class
      * @return
      */
-    public VFSGroupDataset<FImage> loadTrainingData(){
+    public VFSGroupDataset<FImage> loadTrainingData(String dirPath){
 
-        return null;
+        VFSGroupDataset<FImage> classes = null;
+
+        try {
+            classes = new VFSGroupDataset<FImage>(dirPath, ImageUtilities.FIMAGE_READER);
+        } catch (FileSystemException e) {
+            e.printStackTrace();
+        }
+
+        return classes;
 
     }
 
@@ -21,9 +33,17 @@ public class DataLoader {
      * Testing data is unlabelled
      * @return
      */
-    public VFSListDataset<FImage> loadTestingData(){
+    public VFSListDataset<FImage> loadTestingData(String dirPath){
 
-        return null;
+        VFSListDataset<FImage> testing = null;
+
+        try {
+            testing = new VFSListDataset<FImage>(dirPath, ImageUtilities.FIMAGE_READER);
+        } catch (FileSystemException e) {
+            e.printStackTrace();
+        }
+
+        return testing;
 
     }
 
--- COMP3204coursework3/src/main/java/uk/ac/soton/ecs/model/Run1.java
+++ COMP3204coursework3/src/main/java/uk/ac/soton/ecs/model/Run1.java
@@ -1,4 +1,19 @@
 package uk.ac.soton.ecs.model;
 
 public class Run1 {
+
+    public Run1(){
+
+        this.init();
+
+    }
+
+    private void init(){
+
+    }
+
+    public void trainModel(){
+
+    }
+
 }
